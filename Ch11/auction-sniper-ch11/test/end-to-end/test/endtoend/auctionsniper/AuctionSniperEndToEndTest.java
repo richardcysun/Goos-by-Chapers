4650c6d28c45f13a691c603a8db9105622cdf61d
richard_sun <richardsun72@gmail.com>
2019-06-07 18:52:19 +0800

    Initial submission with Ch 10, p.85 codes

---
+++ Ch11/auction-sniper-ch11/test/end-to-end/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,25 @@
+package test.endtoend.auctionsniper;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();                 //Step 1
+        application.startBiddingIn(auction);        //Step 2
+        auction.hasReceivedJoinRequestFromSniper(); //Step 3
+        auction.announceClosed();                   //Step 4
+        application.showSniperHasLostAuction();     //Step 5
+    }
+    
+    @After public void stopAuction() {
+        auction.stop();
+    }
+    
+    @After public void stopApplication() {
+        application.stop();
+    }
+}
