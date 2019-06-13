dfe6e6768420bd1cc2a936e5754bc16efb93a86b
Richard_Sun <richard.sun@msa.hinet.net>
2019-06-13 17:29:11 +0800

    Ch13, p.135, refactor class AuctionMessageTranslator

--- Ch13/auction-sniper-ch13/src/auctionsniper/AuctionMessageTranslator.java
+++ Ch13/auction-sniper-ch13/src/auctionsniper/AuctionMessageTranslator.java
@@ -15,31 +15,64 @@ public class AuctionMessageTranslator implements MessageListener {
         this.listener = listener;
     }
 
+    //Ch13, p.135 revise Ch12, p.120 and p.116 
     public void processMessage(Chat chat, Message message) {
     	//listener.auctionClosed() was added in P.116, but it broke the test of notifiesBidDetailWhenCurrentPriceMessageReceived(), 
     	//so it was commented out and replaced with correct uses 
         //listener.auctionClosed();
-        HashMap<String, String> event = unpackEventFrom(message);
+        //HashMap<String, String> event = unpackEventFrom(message);
+        AuctionEvent event = AuctionEvent.from(message.getBody());
         
-        String type = event.get("Event");
-        if ("CLOSE".equals(type) ) {
+        String eventType = event.type();
+        if ("CLOSE".equals(eventType) ) {
         	//It causes Main to display "Lost"
             listener.auctionClosed();
-        } else if ("PRICE".equals(type)) {
-        	
-        	//Main hasn't implemented currentPrice
-            listener.currentPrice(Integer.parseInt(event.get("CurrentPrice")), 
-                    Integer.parseInt(event.get("Increment")));
+        } else if ("PRICE".equals(eventType)) {
+        	//It causes Main to display "Bidding"
+            listener.currentPrice(event.currentPrice(), event.increment());
         }
     }
-
-    private HashMap<String, String> unpackEventFrom(Message message) {
-        // TODO Auto-generated method stub
-        HashMap<String, String> event = new HashMap<String, String>();
-        for (String element : message.getBody().split(";")) {
-            String[] pair = element.split(":");
-            event.put(pair[0].trim(), pair[1].trim());
+    
+    //Nested Class
+    //Ch13, p.135
+    private static class AuctionEvent {
+        private final HashMap<String, String> fields = new HashMap<String, String>();
+        
+        private String get(String fieldName) {
+            return fields.get(fieldName);
         }
-        return event;
+        
+        private int getInt(String fieldName) {
+            return Integer.parseInt(get(fieldName));
+        }
+        
+        public String type() {
+            return get("Event");
+        }
+        
+        public int currentPrice() {
+            return getInt("CurrentPrice");
+        }
+        
+        public int increment() {
+            return getInt("Increment");
+        }
+        
+        private void addField(String field) {
+            String[] pair = field.split(":");
+            fields.put(pair[0].trim(), pair[1].trim());
+        }
+        
+        static String[] fieldsIn(String messageBody) {
+            return messageBody.split(";");
+        }
+        
+        static AuctionEvent from(String messageBody) {
+            AuctionEvent event = new AuctionEvent();
+            for (String field : fieldsIn(messageBody)) {
+                event.addField(field);
+            }
+            return event;
+        }
     }
 }
